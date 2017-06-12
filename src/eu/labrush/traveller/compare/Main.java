9f400ec2ce698d7df11201969bdfa662201125bd
abocquet <adrienbocquet38@gmail.com>
2017-06-12 21:12:51 +0200

    Last commit

--- src/eu/labrush/traveller/compare/Main.java
+++ src/eu/labrush/traveller/compare/Main.java
@@ -7,19 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String[] problems = new String[]{ "eil51", "eil76", "lin105", "pr107", "pr124", "d198", "lin318", "p654"};
 
-        PointSetFactory factory = new PointSetFactory();
-        PointSet problem = factory.getSet("rat783");
+        for (String p: problems) {
+            PointSetFactory factory = new PointSetFactory();
+            PointSet problem = factory.getSet(p);
 
+            System.out.println("\n" + p);
 
-        //TSPSolver solver = new GeneticSolver();
-        //System.out.println(solver.solveSet(problem, 0.1, 24 * 360 * 60_000));
-
-
-        TSPSolver[] solvers = new TSPSolver[]{new GeneticSolver(), new AnnealingSolver()};
-        for (TSPSolver s: solvers){
-            System.out.print(s.getClass().getSimpleName() + " : ");
-            System.out.println(s.solveSet(problem, 0.1, 24 * 360 * 60_000));
+            TSPSolver[] solvers = new TSPSolver[]{new GeneticSolver(), new AnnealingSolver()};
+            for (TSPSolver s : solvers) {
+                System.out.print(s.getClass().getSimpleName() + " : ");
+                System.out.println(s.solveSet(problem, 0.1, 24 * 360 * 60_000));
+            }
         }
 
     }
